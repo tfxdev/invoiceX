1e940da9e8d344ee76b36676831834f4e687ce6b
Toufic Ahammed <toufic064@gmail.com>
2026-09-19 22:29:16 +0600

    Update comment for DEFAULT_AUTO_FIELD in settings.py

--- invoicex/settings.py
+++ invoicex/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
+# Default primary key field type 
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
